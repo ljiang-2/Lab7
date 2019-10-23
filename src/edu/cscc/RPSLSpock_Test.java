a393002c23713a5dac2b1d2ea7dcaf11cbc29b80
Lauren Jiang <ljiang2@student.cscc.edu>
2019-10-23 12:54:31 -0400

    Initial commit

---
+++ src/edu/cscc/RPSLSpock_Test.java
@@ -0,0 +1,37 @@
+package edu.cscc;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class RPSLSpock_Test {
+    @Test
+    public void testIsValidPick() {
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.ROCK));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.PAPER));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.SCISSORS));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.LIZARD));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.SPOCK));
+        Assert.assertFalse(RPSLSpock.isValidPick(null));
+    }
+    @Test
+    public void testGeneratePick() {
+        String pick = RPSLSpock.generatePick();
+        Assert.assertNotNull(RPSLSpock.isValidPick(pick));
+
+    }
+
+    @Test
+    public void testIsComputerWin() {
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.ROCK, RPSLSpock.SCISSORS));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SCISSORS, RPSLSpock.PAPER));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.PAPER, RPSLSpock.ROCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SPOCK, RPSLSpock.SCISSORS));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.LIZARD, RPSLSpock.SPOCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.ROCK, RPSLSpock.LIZARD));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SPOCK, RPSLSpock.ROCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.LIZARD, RPSLSpock.PAPER));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SCISSORS, RPSLSpock.LIZARD));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.PAPER, RPSLSpock.SPOCK));
+        Assert.assertFalse(RPSLSpock.isComputerWin(RPSLSpock.SCISSORS, RPSLSpock.ROCK));
+    }
+}
